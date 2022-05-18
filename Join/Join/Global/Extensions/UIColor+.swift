9a3f1c749fee62b8fd433e982a51a15f80473cd1
yoonjinlee <yoonjinifer@gmail.com>
2022-05-19 01:31:21 +0900

    update: login 화면 디자인반영하기

--- Join/Join/Global/Extensions/UIColor+.swift
+++ Join/Join/Global/Extensions/UIColor+.swift
@@ -9,6 +9,9 @@ import UIKit
 
 extension UIColor {
     // grayScaleBlack이나 grayScaleWhite는 기본값써도 될듯
+    // grayScale900이 화면 배경색
+    // 일단 피그마 색상샘플이름대로 만들어놓았는데 네이밍 정해지거나 이름 별로면 변경해도됩니다
+    static let grayScale900 = UIColor(red: 45/255, green: 45/255, blue: 45/255, alpha: 1)
     static let grayScale800 = UIColor(red: 70/255, green: 70/255, blue: 70/255, alpha: 1)
     static let grayScale700 = UIColor(red:95/255, green: 95/255, blue: 95/255, alpha: 1)
     static let grayScale600 = UIColor(red: 132/255, green: 132/255, blue: 132/255, alpha: 1)
@@ -16,7 +19,8 @@ extension UIColor {
     static let grayScale400 = UIColor(red: 192/255, green: 192/255, blue: 192/255, alpha: 1)
     static let grayScale300 = UIColor(red: 215/255, green: 215/255, blue: 215/255, alpha: 1)
     static let grayScale200 = UIColor(red: 238/255, green: 238/255, blue: 238/255, alpha: 1)
-
+    static let grayScale100 = UIColor(red: 247/255, green: 247/255, blue: 247/255, alpha: 1)
+    
     static let red100 = UIColor(red: 255/255, green: 110/255, blue: 90/255, alpha: 1)
     static let blue100 = UIColor(red: 113/255, green: 170/255, blue: 255/255, alpha: 1)
     static let pink100 = UIColor(red: 239/255, green: 134/255, blue: 235/255, alpha: 1)
