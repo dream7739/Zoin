d37ed4c19f696b684ec068b2a5433aa21be5d3f0
yoonjinlee <yoonjinifer@gmail.com>
2022-05-19 00:09:15 +0900

    add: uicolor extension 추가하기

--- Join/Join/Global/Extensions/UIColor+.swift
+++ Join/Join/Global/Extensions/UIColor+.swift
@@ -8,5 +8,21 @@
 import UIKit
 
 extension UIColor {
+    // grayScaleBlack이나 grayScaleWhite는 기본값써도 될듯
+    static let grayScale800 = UIColor(red: 70/255, green: 70/255, blue: 70/255, alpha: 1)
+    static let grayScale700 = UIColor(red:95/255, green: 95/255, blue: 95/255, alpha: 1)
+    static let grayScale600 = UIColor(red: 132/255, green: 132/255, blue: 132/255, alpha: 1)
+    static let grayScale500 = UIColor(red: 168/255, green: 168/255, blue: 168/255, alpha: 1)
+    static let grayScale400 = UIColor(red: 192/255, green: 192/255, blue: 192/255, alpha: 1)
+    static let grayScale300 = UIColor(red: 215/255, green: 215/255, blue: 215/255, alpha: 1)
+    static let grayScale200 = UIColor(red: 238/255, green: 238/255, blue: 238/255, alpha: 1)
 
+    static let red100 = UIColor(red: 255/255, green: 110/255, blue: 90/255, alpha: 1)
+    static let blue100 = UIColor(red: 113/255, green: 170/255, blue: 255/255, alpha: 1)
+    static let pink100 = UIColor(red: 239/255, green: 134/255, blue: 235/255, alpha: 1)
+    static let green100 = UIColor(red: 144/255, green: 219/255, blue: 118/255, alpha: 1)
+    static let orange100 = UIColor(red: 255/255, green: 153/255, blue: 0/255, alpha: 1)
+    static let yellow300 = UIColor(red: 251/255, green: 199/255, blue: 13/255, alpha: 1)
+    static let yellow200 = UIColor(red: 255/255, green: 215/255, blue: 72/255, alpha: 1)
+    static let yellow100 = UIColor(red: 255/255, green: 233/255, blue: 156/255, alpha: 1)
 }
