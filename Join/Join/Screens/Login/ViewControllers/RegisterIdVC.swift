cad1a401b297beb11916b760c27c301c34b53b88
yoonjinlee <yoonjinifer@gmail.com>
2022-05-24 01:10:31 +0900

    update: 아이디 등록화면 디자인 반영

--- Join/Join/Screens/Login/ViewControllers/RegisterIdVC.swift
+++ Join/Join/Screens/Login/ViewControllers/RegisterIdVC.swift
@@ -17,36 +17,45 @@ class RegisterIdVC: BaseViewController {
 
     private let titleFirstLabel = UILabel().then {
         $0.text = "친구가 나를 찾을때 필요한"
-        $0.textColor = .black
+        $0.textColor = .grayScale100
+        $0.font = .minsans(size: 24, family: .Bold)
     }
 
     private let titleSecondLabel = UILabel().then {
         $0.text = "아이디를 등록해 주세요.🔍"
-        $0.textColor = .black
+        $0.textColor = .grayScale100
+        $0.font = .minsans(size: 24, family: .Bold)
     }
 
     private let guideLabel = UILabel().then {
         $0.text = "아이디"
-        $0.textColor = .black
+        $0.textColor = .grayScale100
+        $0.font = .minsans(size: 14, family: .Medium)
     }
-    private let emailTextField = UITextField().then {
-        $0.placeholder = "영문 8글자"
-        $0.tintColor = .black
-        $0.backgroundColor = .lightGray
-        $0.borderStyle = .roundedRect
+
+    private let idTextField = UITextField().then {
+        $0.placeholder = "영문 8자 이상 12자 이하"
+        $0.setPlaceHolderColor(.grayScale600)
+        $0.tintColor = .yellow200
+        $0.textColor = .yellow200
+        $0.font = .minsans(size: 16, family: .Medium)
+        $0.backgroundColor = .grayScale800
+        $0.layer.cornerRadius = 20
         $0.addLeftPadding()
     }
 
     private let statusLabel = UILabel().then {
-        $0.text = "사용 가능한 아이디입니다."
-        $0.textColor = .black
+        $0.text = "사용할 수 없는 아이디입니다."
+        $0.textColor = .red100
+        $0.font = .minsans(size: 12, family: .Medium)
     }
 
     private let guideButton = UIButton().then {
-        $0.backgroundColor = .lightGray
-        $0.setTitleColor(.black, for: .normal)
+        $0.backgroundColor = .yellow200
+        $0.setTitleColor(.grayScale900, for: .normal)
         $0.layer.cornerRadius = 16
         $0.setTitle("다음", for: .normal)
+        $0.titleLabel?.font = .minsans(size: 16, family: .Bold)
         // 사용가능한 이메일일때
         // isEnabled, isSelected 설정해놓기
     }
@@ -71,26 +80,24 @@ extension RegisterIdVC {
             titleFirstLabel,
             titleSecondLabel,
             guideLabel,
-            emailTextField,
+            idTextField,
             statusLabel,
             guideButton
         ])
         titleFirstLabel.snp.makeConstraints { (make) in
             make.leading.equalToSuperview().offset(24)
-            make.width.equalTo(250)
-            make.top.equalToSuperview().offset(8)
+            make.top.equalToSuperview().offset(24)
         }
         titleSecondLabel.snp.makeConstraints { (make) in
             make.top.equalTo(titleFirstLabel.snp.bottom).offset(6)
-            make.width.equalTo(250)
             make.leading.equalTo(titleFirstLabel.snp.leading)
         }
         guideLabel.snp.makeConstraints { (make) in
             make.leading.equalTo(titleSecondLabel.snp.leading)
             make.top.equalTo(titleSecondLabel.snp.bottom).offset(25)
-            make.width.equalTo(50)
+            make.width.equalTo(100)
         }
-        emailTextField.snp.makeConstraints { (make) in
+        idTextField.snp.makeConstraints { (make) in
             make.leading.equalTo(guideLabel.snp.leading)
             make.trailing.equalToSuperview().offset(-24)
             make.top.equalTo(guideLabel.snp.bottom).offset(9)
@@ -99,8 +106,7 @@ extension RegisterIdVC {
         }
         statusLabel.snp.makeConstraints { (make) in
             make.leading.equalToSuperview().offset(41)
-            make.top.equalTo(emailTextField.snp.bottom).offset(4)
-            make.width.equalTo(200)
+            make.top.equalTo(idTextField.snp.bottom).offset(4)
         }
         guideButton.snp.makeConstraints { (make) in
             make.leading.equalToSuperview().offset(24)
@@ -123,6 +129,24 @@ extension RegisterIdVC {
 
     private func bind() {
         // TODO: - 텍스트필드 입력 시 영어 8글자 제한걸기
+        idTextField.rx.text
+            .do{ [weak self] text in
+                guard let self = self,
+                      let text = text
+                else { return }
+                if text.count > 0  {
+                    self.idTextField.layer.borderColor = UIColor.grayScale400.cgColor
+                    self.idTextField.layer.cornerRadius = 20
+                    self.idTextField.layer.borderWidth = 2.0
+                } else {
+                    self.idTextField.layer.borderWidth = 0.0
+                }
+            }
+            .subscribe(onNext:  { [weak self] _ in
+
+            })
+            .disposed(by: disposeBag)
+
         guideButton.rx.tap
             .subscribe(onNext: { [weak self] _ in
                 guard let self = self else { return }
@@ -135,13 +159,19 @@ extension RegisterIdVC {
             guard let self = self else { return }
             UIView.animate(withDuration: 0) {
                 if keyboardHeight == 0 {
+                    self.guideButton.layer.cornerRadius = 16
                     self.guideButton.snp.updateConstraints { make in
+                        make.leading.equalToSuperview().offset(24)
+                        make.trailing.equalToSuperview().offset(-24)
                         make.bottom.equalToSuperview().offset(-30)
                     }
                 } else {
+                    self.guideButton.layer.cornerRadius = 0
                     let totalHeight = keyboardHeight - self.view.safeAreaInsets.bottom
                     self.guideButton.snp.updateConstraints { (make) in
-                        make.bottom.equalToSuperview().offset(-totalHeight+(-60))
+                        make.leading.equalToSuperview().offset(0)
+                        make.trailing.equalToSuperview().offset(0)
+                        make.bottom.equalToSuperview().offset(-totalHeight+(-30))
                     }
                 }
                 self.view.layoutIfNeeded()
