5bda48f84ef3c887cae7e5e79a14a4752fb84d14
yoonjinlee <yoonjinifer@gmail.com>
2022-06-09 01:32:51 +0900

    update: profile 화면 초기 세팅 (배경색 및 네비게이션 바 버튼 반영)

--- Join/Join/Screens/Profile/ProfileVC.swift
+++ Join/Join/Screens/Profile/ProfileVC.swift
@@ -7,23 +7,56 @@
 
 import UIKit
 
+import SnapKit
+import Then
+import RxCocoa
+import RxSwift
+
 class ProfileVC: BaseViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setLayout()
         // Do any additional setup after loading the view.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setUpNavigation()
     }
-    */
 
+
+}
+
+extension ProfileVC {
+    private func setLayout() {
+        view.backgroundColor = .grayScale900
+        view.isOpaque = true
+        view.adds([
+
+        ])
+    }
+
+    private func setUpNavigation() {
+        guard let navigationBar = navigationController?.navigationBar else { return }
+        navigationBar.isHidden = false
+        navigationBar.barTintColor = .white
+        navigationBar.shadowImage = UIImage()
+        navigationBar.isTranslucent = false
+        navigationItem.hidesBackButton = true
+        let passButton = UIBarButtonItem(title: "건너뛰기", style: .plain, target: self, action: #selector(moveLast))
+        navigationItem.rightBarButtonItem = passButton
+        navigationItem.rightBarButtonItem?.setTitleTextAttributes([
+            NSAttributedString.Key.font: UIFont.minsans(size: 18, family: .Bold) ?? UIFont.systemFont(ofSize: 18),
+            NSAttributedString.Key.foregroundColor: UIColor.white
+        ], for: .normal)
+    }
+
+    private func bind() {
+
+    }
+
+    @objc func moveLast() {
+
+    }
 }
