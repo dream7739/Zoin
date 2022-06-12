8e9b11ab085b152fda70789a3f254fcfe98015fc
yoonjinlee <yoonjinifer@gmail.com>
2022-06-12 15:45:21 +0900

    update: 프로필화면 디자인 반영하기

--- Join/Join/Screens/Profile/ProfileVC.swift
+++ Join/Join/Screens/Profile/ProfileVC.swift
@@ -14,6 +14,91 @@ import RxSwift
 
 class ProfileVC: BaseViewController {
 
+
+    private let profileBackgroundView = UIView().then {
+        $0.backgroundColor = .grayScale800
+        $0.layer.cornerRadius = 24
+    }
+
+    private let editButton = UIButton().then {
+        $0.setImage(Image.edit, for: .normal)
+    }
+
+    private let profileImageView = UIImageView().then {
+        $0.image = Image.profileDefault
+        $0.layer.cornerRadius = 12
+    }
+
+    private let nicknameLabel = UILabel().then {
+        $0.text = "사용자닉네임자리"
+        $0.textColor = .grayScale100
+        $0.font = .minsans(size: 24, family: .Bold)
+    }
+
+    private let userIdLabel = UILabel().then {
+        $0.text = "사용자id자리"
+        $0.textColor = .grayScale400
+        $0.font = .minsans(size: 16, family: .Medium)
+    }
+
+    private let friendsCountLabel = UILabel().then {
+        $0.text = "123"
+        $0.textColor = .yellow200
+        $0.font = .minsans(size: 20, family: .Bold)
+    }
+
+    private let countSubLabel = UILabel().then {
+        $0.text = "내 친구"
+        $0.textColor = .grayScale100
+        $0.font = .minsans(size: 16, family: .Medium)
+    }
+
+    private let separateView = UIView().then {
+        $0.backgroundColor = .grayScale700
+    }
+
+    private let searchButton = UIButton().then {
+        $0.setImage(Image.searchFriendsButton, for: .normal)
+    }
+
+    private let titleLabel = UILabel().then {
+        $0.text = "내 번개 보관함"
+        $0.textColor = .white
+        $0.font = .minsans(size: 24, family: .Bold)
+    }
+
+    private let boxButton = UIButton().then {
+        $0.layer.cornerRadius = 24
+        $0.setTitle("", for: .normal)
+        $0.backgroundColor = .grayScale800
+    }
+
+    private let closedBoxButton = UIButton().then {
+        $0.layer.cornerRadius = 24
+        $0.setTitle("", for: .normal)
+        $0.backgroundColor = .grayScale800
+    }
+
+    private let boxLabel = UILabel().then {
+        $0.text = "모집 중"
+        $0.textColor = .white
+        $0.font = .minsans(size: 16, family: .Bold)
+    }
+
+    private let closedBoxLabel = UILabel().then {
+        $0.text = "마감"
+        $0.textColor = .white
+        $0.font = .minsans(size: 16, family: .Bold)
+    }
+
+    private let boxImage = UIImageView().then {
+        $0.image = Image.pinkArchive
+    }
+
+    private let closedBoxImage = UIImageView().then {
+        $0.image = Image.greenArchive
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setLayout()
@@ -33,8 +118,103 @@ extension ProfileVC {
         view.backgroundColor = .grayScale900
         view.isOpaque = true
         view.adds([
-
+            profileBackgroundView,
+            titleLabel,
+            boxButton,
+            closedBoxButton
         ])
+        profileBackgroundView.adds([
+            profileImageView,
+            editButton,
+            nicknameLabel,
+            userIdLabel,
+            friendsCountLabel,
+            countSubLabel,
+            separateView,
+            searchButton
+        ])
+        boxButton.adds([
+            boxLabel,
+            boxImage
+        ])
+        closedBoxButton.adds([
+            closedBoxLabel,
+            closedBoxImage
+        ])
+
+        profileBackgroundView.snp.makeConstraints { (make) in
+            make.top.equalToSuperview().offset(16)
+            make.leading.equalToSuperview().offset(24)
+            make.trailing.equalToSuperview().offset(-24)
+            make.centerX.equalToSuperview()
+            make.width.equalTo(327)
+            make.height.equalTo(338)
+        }
+
+        profileImageView.snp.makeConstraints { (make) in
+            make.top.equalToSuperview().offset(24)
+            make.centerX.equalToSuperview()
+            make.size.equalTo(113)
+        }
+
+        editButton.snp.makeConstraints { (make) in
+            make.top.equalToSuperview().offset(16)
+            make.trailing.equalToSuperview().offset(-16)
+            make.size.equalTo(24)
+        }
+
+        nicknameLabel.snp.makeConstraints { (make) in
+            make.top.equalTo(profileImageView.snp.bottom).offset(16)
+            make.centerX.equalToSuperview()
+        }
+
+        userIdLabel.snp.makeConstraints { (make) in
+            make.top.equalTo(nicknameLabel.snp.bottom).offset(2)
+            make.centerX.equalToSuperview()
+        }
+
+        friendsCountLabel.snp.makeConstraints { (make) in
+            make.top.equalTo(userIdLabel.snp.bottom).offset(40)
+            make.leading.equalToSuperview().offset(60)
+        }
+
+        countSubLabel.snp.makeConstraints { (make) in
+            make.top.equalTo(friendsCountLabel.snp.bottom).offset(2)
+            make.leading.equalTo(friendsCountLabel.snp.leading)
+        }
+
+        separateView.snp.makeConstraints { (make) in
+            make.top.equalTo(userIdLabel.snp.bottom).offset(36)
+            make.width.equalTo(2)
+            make.height.equalTo(60)
+            make.leading.equalTo(countSubLabel.snp.trailing).offset(60)
+        }
+
+        searchButton.snp.makeConstraints { (make) in
+            make.top.equalTo(friendsCountLabel.snp.top)
+            make.leading.equalTo(separateView.snp.trailing).offset(24)
+            make.trailing.equalToSuperview().offset(-24)
+            make.width.equalTo(116)
+            make.height.equalTo(43)
+        }
+
+        titleLabel.snp.makeConstraints { (make) in
+            make.top.equalTo(profileBackgroundView.snp.bottom).offset(24)
+            make.leading.equalTo(profileBackgroundView.snp.leading)
+        }
+
+        boxButton.snp.makeConstraints { (make) in
+            make.top.equalTo(titleLabel.snp.bottom).offset(24)
+            make.leading.equalTo(titleLabel.snp.leading)
+            make.size.equalTo(156)
+        }
+
+        closedBoxButton.snp.makeConstraints { (make) in
+            make.top.equalTo(titleLabel.snp.bottom).offset(24)
+            make.leading.equalTo(boxButton.snp.trailing).offset(20)
+            make.trailing.equalToSuperview().offset(-24)
+            make.size.equalTo(156)
+        }
     }
 
     private func setUpNavigation() {
