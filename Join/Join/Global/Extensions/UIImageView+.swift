f0ab5096ee8ca3fd227cd1b2262d39b2f70c7f25
yoonjinlee <yoonjinifer@gmail.com>
2022-12-16 13:55:03 +0900

    add: imageview extension 추가

--- Join/Join/Global/Extensions/UIImageView+.swift
+++ Join/Join/Global/Extensions/UIImageView+.swift
@@ -63,4 +63,21 @@ extension UIImage {
         UIGraphicsEndImageContext()
         return result
     }
+    func resized(withPercentage percentage: CGFloat, isOpaque: Bool = true) -> UIImage? {
+            let canvas = CGSize(width: size.width * percentage, height: size.height * percentage)
+            let format = imageRendererFormat
+            format.opaque = isOpaque
+            return UIGraphicsImageRenderer(size: canvas, format: format).image {
+                _ in draw(in: CGRect(origin: .zero, size: canvas))
+            }
+        }
+        func resized(toWidth width: CGFloat, isOpaque: Bool = true) -> UIImage? {
+            let canvas = CGSize(width: width, height: CGFloat(ceil(width/size.width * size.height)))
+            let format = imageRendererFormat
+            format.opaque = isOpaque
+            return UIGraphicsImageRenderer(size: canvas, format: format).image {
+                _ in draw(in: CGRect(origin: .zero, size: canvas))
+            }
+        }
+
 }
